ba251dfefa66ad8d0a6bc799dbc084bbb0d65e95
yonatan-co <67898615+yonatan-co@users.noreply.github.com>
2022-07-22 15:09:36 +0300

    added update event route

--- api/src/controllers/feed.ts
+++ api/src/controllers/feed.ts
@@ -3,9 +3,15 @@ import { AutherizedRequest } from "../types/requests";
 
 import { Event } from "../models/event.model";
 import { EventToUser } from "../models/eventToUser.model";
+import { ServerError } from "../types/error";
 
+// note: these middlewares don't have authorazition yet.
 export default {
-  createEvent: async (req: any, res: Response, next: NextFunction) => {
+  createEvent: async (
+    req: AutherizedRequest,
+    res: Response,
+    next: NextFunction
+  ) => {
     // just for now.
     const userId = "62d67d46d1b3181b92531de2";
     try {
@@ -34,5 +40,27 @@ export default {
     }
   },
 
-  updateEvent: async (req: Request, res: Response, next: NextFunction) => {},
+  updateEvent: async (req: Request, res: Response, next: NextFunction) => {
+    const eventId = req.params.eventId;
+    const event = await Event.findById(eventId);
+
+    if (!event) {
+      const error: ServerError = new Error("no event found");
+      error.status = 404;
+      throw error;
+    }
+    event.title = req.body.title;
+    event.desc = req.body.desc;
+    event.mainPhoto = req.body.mainPhoto;
+    const updatedEvent = await event.save();
+    res.status(200).json({
+      message: "event updated",
+      event: updatedEvent,
+    });
+  },
+  deleteEvent: async (
+    req: AutherizedRequest,
+    res: Response,
+    next: NextFunction
+  ) => {},
 };
